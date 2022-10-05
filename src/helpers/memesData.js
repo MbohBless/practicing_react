2958ac55a52fb180c955688fef779846a8a36f27
mbohbless <mbohblesspearl@gmail.com>
2022-10-05 13:24:21 +0100

    correction to memes

---
+++ src/helpers/memesData.js
@@ -0,0 +1,44 @@
+export default {
+    "sucess":true,
+    "data":{
+        "memes":[   
+            {
+                "id":"1",
+                "name":"One does not simply",
+                "url":"https://i.imgflip.com/1bij.jpg",
+                "width":1200,
+                "height":1200
+            },
+            {
+                "id":"2",
+                "name":"Ancient Aliens",
+                "url":"https://i.imgflip.com/1ur9b0.jpg",
+                "width":1200,
+                "height":1200
+            },
+            {
+                "id":"3",
+                "name":"X, X Everywhere",
+                "url":"https://i.imgflip.com/1g8my4.jpg",
+                "width":1200,
+                "height":1200
+            },
+            {
+                "id":"4",
+                "name":"Two Buttons",
+                "url":"https://i.imgflip.com/1g8my4.jpg",
+                "width":1200,
+                "height":1200
+            },
+            {
+                "id":"5",
+                "name":"Change My Mind",
+                "url":"https://i.imgflip.com/1g8my4.jpg",
+                "width":1200,
+                "height":1200
+            },
+        ]
+}}
+
+
+    
